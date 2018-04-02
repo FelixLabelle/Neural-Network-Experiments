4d2779322cde4ad40ecea9150048abe4fda61cab
FelixLabelle <flx.labelle@gmail.com>
2018-04-01 23:08:00 -0400

    Stylistic improvements (PEP8 conformity)

--- classifier.py
+++ classifier.py
@@ -1,25 +1,27 @@
 import numpy as np
 
+"""This class implements a classifier with a variable number
+ of hidden layers and neurons for each layer"""
 class classifier:
 
-    # Activation functions are typically sigmoidal functions
-    # https://en.wikipedia.org/wiki/Sigmoid_function
-    # Activation functions are typically sigmoid functions, which means they are
-    # Real, differntiable, bounded and have a non negative first derivative
-
+    """Method implements RELU activation function"""
     def relu(self, node_values):
         return (node_values > 0) * node_values
 
+    """Method implements RELU derivative"""
     def relu_derivative(self, relu_vals):
-        return relu_vals > 0 
+        return relu_vals > 0
 
+    """Method implements tanh derivative"""
     def tanh_derivative(self, tanh_vals):
         return 1 - np.power(tanh_vals, 2)
 
+    """Method implements sigmoid derivative"""
     def sigmoid_derivative(self, sigmoid_vals):
         return sigmoid_vals*(1-sigmoid_vals)
 
-    def sigmoid(self,node_values):
+    """Method implements sigmoid activation function"""
+    def sigmoid(self, node_values):
         # https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
         return 1/(1+np.exp(-node_values)) # Todo how to combat numerical issues?
 
@@ -30,17 +32,19 @@ class classifier:
         self.Y = np.array([])
         self.num_examples = 0
 
-    def loadData(self, x, y):
+    """Method loads training data"""
+    def load_data(self, x, y):
         self.X = x
         self.Y = y
         self.num_examples = len(self.X)  # training set size
 
-    def configureNeuralNetwork(self, numberOfInputs, numberOfClasses, numberOfHiddenNeurons = 5,
-                               epsilon = 1e-5, reg_lambda = 1e-2, activationFunction = "tanh"):
+    """Sets training and neural network configurations"""
+    def configure_classifier(self, number_of_inputs, number_of_classes, hidden_layers = 5,
+                               epsilon = 1e-5, reg_lambda = 1e-2, activation_function = "tanh"):
         # Gradient descent parameters, play with these and see their effects
         self.epsilon = epsilon
         self.reg_lambda = reg_lambda
-        self.layers = [numberOfInputs] + numberOfHiddenNeurons + [numberOfClasses] # rewrite this as a numpy array
+        self.layers = [number_of_inputs] + hidden_layers + [number_of_classes] # rewrite this as a numpy array
         # find how to concatenate the middle one properly
         self.model = {}
         # creates a random matrix that is inputs by outputs
@@ -50,13 +54,13 @@ class classifier:
         self.a = [np.zeros((1, self.layers[i+1])) for i in range(len(self.layers)-1)]
 
         # Consider passing function via arguments
-        if activationFunction == 'tanh':
+        if activation_function == 'tanh':
             self.activation_function = np.tanh
             self.activation_derivative = self.tanh_derivative
-        elif activationFunction == 'relu':
+        elif activation_function == 'relu':
             self.activation_function = self.relu
             self.activation_derivative = self.relu_derivative
-        elif activationFunction == 'sigmoid':
+        elif activation_function == 'sigmoid':
             self.activation_function = self.sigmoid
             self.activation_derivative = self.sigmoid_derivative
         else:
@@ -64,7 +68,7 @@ class classifier:
             self.activation_derivative = self.relu_derivative
 
 
-    # This function calculates the cost function and backpropagates the erros
+    """This function calculates the cost function and backpropagates the error"""
     def train_model(self, num_passes, print_loss=False):
 
         # Gradient descent. For each batch
@@ -99,10 +103,11 @@ class classifier:
 
         #return [self.weights,self.biases]
 
+    """ Predicts output values of a given dataset"""
     def predict(self, x):
         return np.argmax(self.forward_prop(x), axis=1)
 
-    # Evaluate error in the model
+    """ Evaluate error in the model """
     def calculate_loss(self):
         probs = self.forward_prop(self.X)
         # Calculating the loss
@@ -114,6 +119,7 @@ class classifier:
         return 1. / self.num_examples * data_loss
 
     # TODO make this method private
+    """ Evaluates forward propagation for a given set x"""
     def forward_prop(self, x):
         z = x.dot(self.weights[0]) + self.biases[0]
         self.a[0] = self.activation_function(z)
