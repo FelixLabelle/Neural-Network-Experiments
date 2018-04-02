fcb7f2850fb95bfdb0c479f7098e4cff1649ada9
FelixLabelle <flx.labelle@gmail.com>
2018-04-02 08:06:38 -0400

    Added minibatch and SGD

--- classifier.py
+++ classifier.py
@@ -1,27 +1,30 @@
 import numpy as np
+import random
 
-"""This class implements a classifier with a variable number
- of hidden layers and neurons for each layer"""
 class classifier:
+    """This class implements a classifier with a variable number
+     of hidden layers and neurons for each layer"""
 
-    """Method implements RELU activation function"""
     def relu(self, node_values):
+        """Method implements RELU activation function"""
         return (node_values > 0) * node_values
 
-    """Method implements RELU derivative"""
+
     def relu_derivative(self, relu_vals):
+        """Method implements RELU derivative"""
         return relu_vals > 0
 
-    """Method implements tanh derivative"""
+
     def tanh_derivative(self, tanh_vals):
+        """Method implements tanh derivative"""
         return 1 - np.power(tanh_vals, 2)
 
-    """Method implements sigmoid derivative"""
     def sigmoid_derivative(self, sigmoid_vals):
+        """Method implements sigmoid derivative"""
         return sigmoid_vals*(1-sigmoid_vals)
 
-    """Method implements sigmoid activation function"""
     def sigmoid(self, node_values):
+        """Method implements sigmoid activation function"""
         # https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
         return 1/(1+np.exp(-node_values)) # Todo how to combat numerical issues?
 
@@ -32,15 +35,19 @@ class classifier:
         self.Y = np.array([])
         self.num_examples = 0
 
-    """Method loads training data"""
+
     def load_data(self, x, y):
+        """Method loads training data"""
         self.X = x
         self.Y = y
         self.num_examples = len(self.X)  # training set size
+        if self.batch_size == -1:
+            self.batch_size = len(self.X)
 
-    """Sets training and neural network configurations"""
     def configure_classifier(self, number_of_inputs, number_of_classes, hidden_layers = 5,
-                               epsilon = 1e-5, reg_lambda = 1e-2, activation_function = "tanh"):
+                               epsilon = 1e-5, reg_lambda = 1e-2, activation_function = "tanh",
+                             batch_size = -1):
+        """Sets training and neural network configurations"""
         # Gradient descent parameters, play with these and see their effects
         self.epsilon = epsilon
         self.reg_lambda = reg_lambda
@@ -52,6 +59,7 @@ class classifier:
                                         self.layers[i+1]) / np.sqrt(self.layers[i]) for i in range(len(self.layers)-1)]
         self.biases = [np.zeros((1, self.layers[i+1])) for i in range(len(self.layers)-1)]
         self.a = [np.zeros((1, self.layers[i+1])) for i in range(len(self.layers)-1)]
+        self.batch_size = batch_size
 
         # Consider passing function via arguments
         if activation_function == 'tanh':
@@ -68,19 +76,22 @@ class classifier:
             self.activation_derivative = self.relu_derivative
 
 
-    """This function calculates the cost function and backpropagates the error"""
-    def train_model(self, num_passes, print_loss=False):
 
+    def train_model(self, num_passes, print_loss=False):
+        """This function calculates the cost function and backpropagates the error"""
         # Gradient descent. For each batch
         # TODO implement mini-batch and SGD
         for i in range(0, num_passes):
+            selection_array = random.sample(range(self.num_examples), self.batch_size)
+            batch_input = self.X[selection_array] # make sure this gets the whole set
+            batch_output = self.Y[selection_array]
 
-            probs = self.forward_prop(self.X)
+            probs = self.__forward_prop__(batch_input)
 
             dW = [np.zeros(self.weights[i].shape) for i in range(len(self.layers)-1)]
             db = [np.zeros((1, self.layers[i + 1])) for i in range(len(self.layers) - 1)]
             derivative = probs
-            derivative[range(self.num_examples), self.Y] -= 1
+            derivative[range(self.batch_size),batch_output] -= 1
 
             for i in range(len(self.layers) - 2,0,-1):
                 dW[i] = (self.a[i-1].T).dot(derivative)
@@ -88,7 +99,7 @@ class classifier:
                 db[i] = np.sum(derivative, axis=0, keepdims=True)
                 derivative = derivative.dot(self.weights[i].T) * self.activation_derivative(self.a[i-1])
 
-            dW[0] = np.dot(self.X.T, derivative)
+            dW[0] = np.dot(batch_input.T, derivative)
             dW[0] += self.reg_lambda * self.weights[0]
             db[0] = np.sum(derivative, axis=0)
 
@@ -103,13 +114,14 @@ class classifier:
 
         #return [self.weights,self.biases]
 
-    """ Predicts output values of a given dataset"""
     def predict(self, x):
-        return np.argmax(self.forward_prop(x), axis=1)
+        """ Predicts output values of a given dataset"""
+        return np.argmax(self.__forward_prop__(x), axis=1)
 
-    """ Evaluate error in the model """
+
     def calculate_loss(self):
-        probs = self.forward_prop(self.X)
+        """ Evaluate error in the model """
+        probs = self.__forward_prop__(self.X)
         # Calculating the loss
         corect_logprobs = -np.log(probs[range(self.num_examples), self.Y])
         data_loss = np.sum(corect_logprobs)
@@ -118,9 +130,8 @@ class classifier:
         # Add regularization this life back later
         return 1. / self.num_examples * data_loss
 
-    # TODO make this method private
-    """ Evaluates forward propagation for a given set x"""
-    def forward_prop(self, x):
+    def __forward_prop__(self, x):
+        """ Evaluates forward propagation for a given set x"""
         z = x.dot(self.weights[0]) + self.biases[0]
         self.a[0] = self.activation_function(z)
         for i in range(1,len(self.layers) - 1):
