fc0c85f66bd5ad499e1207341c456c09efc412ae
Felix Labelle <flx.labelle@gmail.com>
2018-04-10 08:21:50 -0400

    Correctly generate train and validation data

--- example.py
+++ example.py
@@ -6,8 +6,10 @@ from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original')
 input_data = preprocessing.scale(np.c_[mnist.data])
 target_class = np.concatenate(np.c_[mnist.target],axis=0).astype(int)
-training_values = np.random.randint(0,len(input_data)-1,5000)
-validation_values = np.random.randint(0,len(input_data)-1,250)
+random_indices = np.arange(len(input_data))
+np.random.shuffle(random_indices)
+training_values = random_indices[0:5000]
+validation_values = random_indices[5000:5250]
 training_inputs = input_data[training_values]
 training_outputs = target_class[training_values]
 validation_inputs = input_data[validation_values]
@@ -28,7 +30,8 @@ ann = classifier()
 ann.configure_classifier(input_dim,output_dim,hidden_layers =numberOfNeurons,activation_function='relu',
                          batch_size=500)
 ann.load_data(training_inputs,training_outputs)
-model = ann.train_model(num_iterations=15000,epsilon = 1e-4,anneal="step", annealing_hyperparameters=[20,0.5])
+ann.load_data(training_inputs,training_outputs)
+model = ann.train_model(num_iterations=15000,epsilon = 1e-4,anneal="default", annealing_hyperparameters=[20,0.5])
 predicted_outputs = ann.predict(validation_inputs)
 error = sum((predicted_outputs-validation_outputs) != 0)
 print(predicted_outputs-validation_outputs)
